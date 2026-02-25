eb45a67b8dad0cb7a7d4baf2e8ee6a6cef445d7b
Zhi Wang <zhiw@nvidia.com>
2026-02-25 20:04:48 +0200

    rust: pci: add extended capability and SR-IOV support
    Implement ExtCapability<T> as an Io implementor for PCI extended
    capability regions. It delegates IoCapable to the underlying
    ConfigSpace, so io_project!/io_read!/io_write! work directly on
    capability register structs.
    
    ExtCapId wraps PCI_EXT_CAP_ID_* constants for type-safe capability
    discovery. ExtSriovRegs is a #[repr(C)] layout of the PCIe SR-IOV
    registers, and ExtSriovCapability is a convenience alias for
    ExtCapability<ExtSriovRegs>.
    
    Also add a rust helper for the PCI_EXT_CAP_NEXT() macro.
    
    Link: https://lore.kernel.org/rust-for-linux/20260126215957.541180-1-zhiw@nvidia.com/
    Cc: Gary Guo <gary@garyguo.net>
    Signed-off-by: Zhi Wang <zhiw@nvidia.com>

--- rust/kernel/pci.rs
+++ rust/kernel/pci.rs
@@ -31,10 +31,17 @@ use core::{
     },
 };
 
+mod cap;
 mod id;
 mod io;
 mod irq;
 
+pub use self::cap::{
+    ExtCapId,
+    ExtCapability,
+    ExtSriovCapability,
+    ExtSriovRegs, //
+};
 pub use self::id::{
     Class,
     ClassMask,
