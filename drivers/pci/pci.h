189165f820f09a47770a75898cd6c8f2c890c47f
Peter Colberg <pcolberg@redhat.com>
2026-03-03 16:15:21 -0500

    PCI: add driver flag to opt into disabling SR-IOV on remove()
    Add a flag managed_sriov to the pci_driver structure that allows a
    driver to opt into disabling the Single Root I/O Virtualization (SR-IOV)
    capability of the device when the driver is unbound.
    
    Add a new function pci_iov_disable() that is invoked before the remove()
    callback of a PCI driver and checks for the presence of the new flag.
    If the flag is set, invoke the sriov_configure() callback to allow the
    driver to gracefully disable SR-IOV. Warn if the driver fails to do so
    and forcibly disable SR-IOV using sriov_disable().
    
    Since a (broken) driver may theoretically re-enable SR-IOV during its
    remove() callback, extend pci_iov_remove() to forcibly disable SR-IOV
    after remove() if needed and only if the flag managed_sriov is set.
    
    Altogether the flag ensures that when a Virtual Function (VF) is bound
    to a driver, the corresponding Physical Function (PF) is bound to a
    driver, too, since the VF devices are destroyed when the PF driver is
    unbound. This guarantee is a prerequisite for exposing a safe Rust
    API that allows a VF driver to obtain the PF device for a VF device
    and subsequently access the device private data of the PF device.
    
    Suggested-by: Danilo Krummrich <dakr@kernel.org>
    Signed-off-by: Peter Colberg <pcolberg@redhat.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -943,6 +943,7 @@ static inline void pci_restore_pasid_state(struct pci_dev *pdev) { }
 #ifdef CONFIG_PCI_IOV
 int pci_iov_init(struct pci_dev *dev);
 void pci_iov_release(struct pci_dev *dev);
+void pci_iov_disable(struct pci_dev *dev);
 void pci_iov_remove(struct pci_dev *dev);
 void pci_iov_update_resource(struct pci_dev *dev, int resno);
 resource_size_t pci_sriov_resource_alignment(struct pci_dev *dev, int resno);
@@ -977,6 +978,7 @@ static inline int pci_iov_init(struct pci_dev *dev)
 	return -ENODEV;
 }
 static inline void pci_iov_release(struct pci_dev *dev) { }
+static inline void pci_iov_disable(struct pci_dev *dev) { }
 static inline void pci_iov_remove(struct pci_dev *dev) { }
 static inline void pci_iov_update_resource(struct pci_dev *dev, int resno) { }
 static inline resource_size_t pci_sriov_resource_alignment(struct pci_dev *dev,
