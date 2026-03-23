14f94bbe4be14112d99b8891f1db5ebca3e4d27c
Gary Guo <gary@garyguo.net>
2026-03-23 15:37:59 +0000

    gpu: nova-core: use I/O projection for cleaner encapsulation
    Use `io_project!` for PTE array and message queues to remove the
    break off encapsulation.
    
    The remaining `dma_read!` and `dma_write!` is now only acting on
    primitives; thus replace by `io_read!` and `io_write!`.
    
    Signed-off-by: Gary Guo <gary@garyguo.net>
    Signed-off-by: Zhi Wang <zhiw@nvidia.com>

--- drivers/gpu/nova-core/gsp.rs
+++ drivers/gpu/nova-core/gsp.rs
@@ -12,8 +12,13 @@ use kernel::{
         DmaAddress, //
     },
     fs::file,
+    io::{
+        self,
+        io_project, //
+    },
     pci,
     prelude::*,
+    ptr::KnownSize,
     transmute::{
         AsBytes,
         FromBytes, //
@@ -59,12 +64,20 @@ unsafe impl<const NUM_ENTRIES: usize> FromBytes for PteArray<NUM_ENTRIES> {}
 unsafe impl<const NUM_ENTRIES: usize> AsBytes for PteArray<NUM_ENTRIES> {}
 
 impl<const NUM_PAGES: usize> PteArray<NUM_PAGES> {
-    /// Returns the page table entry for `index`, for a mapping starting at `start`.
-    // TODO: Replace with `IoView` projection once available.
-    fn entry(start: DmaAddress, index: usize) -> Result<u64> {
-        start
-            .checked_add(num::usize_as_u64(index) << GSP_PAGE_SHIFT)
-            .ok_or(EOVERFLOW)
+    /// Initialize a new page table array mapping `NUM_PAGES` GSP pages starting at address `start`.
+    fn init<T: FromBytes + AsBytes + KnownSize + ?Sized>(
+        view: io::View<'_, Coherent<T>, Self>,
+        start: DmaAddress,
+    ) -> Result<()> {
+        for i in 0..NUM_PAGES {
+            io_project!(view, .0[i]).write_val(
+                start
+                    .checked_add(num::usize_as_u64(i) << GSP_PAGE_SHIFT)
+                    .ok_or(EOVERFLOW)?,
+            );
+        }
+
+        Ok(())
     }
 }
 
@@ -147,17 +160,12 @@ impl LogBuffer {
 
         let start_addr = buffer.dma_handle();
 
-        // SAFETY: `buffer` has just been created and we are its sole user.
-        let pte_region = unsafe {
-            &mut buffer.as_mut()[size_of::<u64>()..][..RM_LOG_BUFFER_NUM_PAGES * size_of::<u64>()]
-        };
-
-        // Write values one by one to avoid an on-stack instance of `PteArray`.
-        for (i, chunk) in pte_region.chunks_exact_mut(size_of::<u64>()).enumerate() {
-            let pte_value = PteArray::<0>::entry(start_addr, i)?;
-
-            chunk.copy_from_slice(&pte_value.to_ne_bytes());
-        }
+        let pte_view = io_project!(
+            buffer,
+            [size_of::<u64>()..]?[..RM_LOG_BUFFER_NUM_PAGES * size_of::<u64>()]?
+        )
+        .try_cast::<PteArray<RM_LOG_BUFFER_NUM_PAGES>>()?;
+        PteArray::init(pte_view, start_addr)?;
 
         let (header, header_len) = match build_id {
             Some(bid) => (
