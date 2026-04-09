2a8f06975dee75db9b9d7b6a72fa6e2684d29721
Alexandre Courbot <acourbot@nvidia.com>
2026-04-09 23:58:49 +0900

    gpu: nova-core: switch to kernel bitfield macro
    Replace uses of the Nova-internal `bitfield!` macro by the kernel one,
    and remove the now-unneeded local macro.
    
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>
    Reviewed-by: Eliot Courtney <ecourtney@nvidia.com>
    Signed-off-by: Zhi Wang <zhiw@nvidia.com>

--- drivers/gpu/nova-core/gsp/fw.rs
+++ drivers/gpu/nova-core/gsp/fw.rs
@@ -10,6 +10,7 @@ use r570_144 as r570;
 use core::ops::Range;
 
 use kernel::{
+    bitfield,
     dma::Coherent,
     prelude::*,
     ptr::{
@@ -18,8 +19,8 @@ use kernel::{
         KnownSize, //
     },
     sizes::{
-        SZ_128K,
-        SZ_1M, //
+        SZ_128K, //
+        SZ_1M,
     },
     transmute::{
         AsBytes,
@@ -512,8 +513,8 @@ unsafe impl AsBytes for MsgqRxHeader {}
 
 bitfield! {
     struct MsgHeaderVersion(u32) {
-        31:24 major as u8;
-        23:16 minor as u8;
+        31:24 major;
+        23:16 minor;
     }
 }
 
@@ -522,9 +523,9 @@ impl MsgHeaderVersion {
     const MINOR_TOT: u8 = 0;
 
     fn new() -> Self {
-        Self::default()
-            .set_major(Self::MAJOR_TOT)
-            .set_minor(Self::MINOR_TOT)
+        Self::zeroed()
+            .with_major(Self::MAJOR_TOT)
+            .with_minor(Self::MINOR_TOT)
     }
 }
 
