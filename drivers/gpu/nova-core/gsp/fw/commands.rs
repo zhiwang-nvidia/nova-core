kernelorg-mirror/linux_kernel_git_jfern_linux

gpu: nova-core: gsp: Expose total physical VRAM end from FB region info
Add `total_fb_end()` to `GspStaticConfigInfo` that computes the
exclusive end address of the highest valid FB region covering both
usable and GSP-reserved areas.

This allows callers to know the full physical VRAM extent, not just
the allocatable portion.

Signed-off-by: Joel Fernandes <joelagnelf@nvidia.com>

drivers/gpu/nova-core/gsp/fw/commands.rs
@@ -178,6 +178,13 @@ impl GspStaticConfigInfo {
             }
         })
     }
+
+    /// Compute the end of physical VRAM from all FB regions.
+    pub(crate) fn total_fb_end(&self) -> Option<u64> {
+        self.fb_regions()
+            .map(|reg| reg.limit.saturating_add(1))
+            .max()
+    }
 }
 
 // SAFETY: Padding is explicit and will not contain uninitialized data.

zhiwang-nvidia/nova-core

Signed-off-by: Zhi Wang <zhiw@nvidia.com>

drivers/gpu/nova-core/gsp/fw/commands.rs
@@ -177,6 +177,13 @@ impl GspStaticConfigInfo {
             }
         })
     }
+
+    /// Compute the end of physical VRAM from all FB regions.
+    pub(crate) fn total_fb_end(&self) -> Option<u64> {
+        self.fb_regions()
+            .filter_map(|reg| reg.limit.checked_add(1))
+            .max()
+    }
 }
 
 // SAFETY: Padding is explicit and will not contain uninitialized data.